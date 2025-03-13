3a2aba8ef0bbc0a36c018085514c2da31c77d65e
Gabriela Reyes <greyesromero.ios@gmail.com>
2025-03-13 16:29:05 -0600

    Config Files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}',
+      './public/index.html',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'brand-purple': '#9b4de0',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
